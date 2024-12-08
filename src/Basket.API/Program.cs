41d3701cd2c875a8d7a340a5c538c30f4d01cc4b
JeffreyKayfes <jeffreykayfes@gmail.com>
2024-12-08 17:26:49 -0600

    First test commit

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -1,4 +1,5 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿// Test commit for github
+var builder = WebApplication.CreateBuilder(args);
 
 builder.AddBasicServiceDefaults();
 builder.AddApplicationServices();
